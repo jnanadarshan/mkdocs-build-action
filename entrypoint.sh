9b9118e1fde5d25f74cd9cb8c61266f0dbf6e9d7
Jnanadarshan Nayak <github@jdnayak.com>
2020-04-14 19:16:17 +0530

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -x # verbose mode
+set -e # stop executing after error
+
+echo "Starting mkdocs build"
+
+####################################################
+# Set workspace permissions
+####################################################
+
+chmod -R a+w /github/workspace
+
+####################################################
+# Build the Mkdocs site
+####################################################
+
+mkdocs build --trace
+
+####################################################
+# Build completed
+####################################################
+
+echo "Completed mkdocs build"
